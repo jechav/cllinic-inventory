19096845eaa3da0a9adfa329565d9c7fe2a0c99a
jochechavez <joche.echavezm@gmail.com>
2015-08-31 07:01:03 -0500

    initial admin custom: products

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Product, Location, Brand, Manufacture, Accessory 
 
+
+class productAdmin(admin.ModelAdmin):
+    list_display = (
+            'name', 'model', 'series', 'voltage',
+            'property', 
+    )
+
+    #list_filter = ('load',)
+
+    search_fields = (
+            #'tag','activation_date', 'expriry_date',
+    )
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, productAdmin)
 admin.site.register(Location)
 admin.site.register(Brand)
 admin.site.register(Manufacture)
